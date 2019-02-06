355e340c5d65eaa0ce9e18cc777145df358c14b7
Tomer Shmul <tomershmul@gmail.com>
2019-02-06 02:08:09 +0200

    add try except to data_loader

--- code/data_loader.py
+++ code/data_loader.py
@@ -23,29 +23,34 @@ class TrainImageFolder(data.Dataset):
         self.data_dir=data_dir
         self.model_output_size = model_output_size
     def __getitem__(self, index):
-        img=Image.open(self.data_dir+'/'+self.file_list[index])
-        if self.transform is not None:
-            # img_original = self.transform(img)
-            # img_resize=transforms.Resize(self.model_output_size)(img_original)
-            # img_original = np.asarray(img_original)
+        try:
+            img=Image.open(self.data_dir+'/'+self.file_list[index])
+            if self.transform is not None:
+                # img_original = self.transform(img)
+                # img_resize=transforms.Resize(self.model_output_size)(img_original)
+                # img_original = np.asarray(img_original)
 
-            img_resize=transforms.Resize(self.model_output_size)(img)
-            img_original = np.asarray(img)
+                img_resize=transforms.Resize(self.model_output_size)(img)
+                img_original = np.asarray(img)
 
-            #print('image ', self.file_list[index],img_original.shape)
-            if img_resize.mode != 'RGB':
-                img_resize = img_resize.convert('RGB')
-            img_lab = rgb2lab(img_resize)
-            #img_lab = (img_lab + 128) / 255
-            img_ab = img_lab[:, :, 1:3]
-            img_ab = torch.from_numpy(img_ab.transpose((2, 0, 1)))
-            #print('img_ori',img_original.shape)
-            #print('img_ab',img_ab.size())
-            if img_original.ndim != 3:
-                img_original = gray2rgb(img_original)
-            img_original = rgb2lab(img_original)[:,:,0]-50.
-            img_original = torch.from_numpy(img_original)
-            return img_original, img_ab
+                #print('image ', self.file_list[index],img_original.shape)
+                if img_resize.mode != 'RGB':
+                    img_resize = img_resize.convert('RGB')
+                img_lab = rgb2lab(img_resize)
+                #img_lab = (img_lab + 128) / 255
+                img_ab = img_lab[:, :, 1:3]
+                img_ab = torch.from_numpy(img_ab.transpose((2, 0, 1)))
+                #print('img_ori',img_original.shape)
+                #print('img_ab',img_ab.size())
+                if img_original.ndim != 3:
+                    img_original = gray2rgb(img_original)
+                img_original = rgb2lab(img_original)[:,:,0]-50.
+                img_original = torch.from_numpy(img_original)
+                return img_original, img_ab
+        except Exception as ex:
+            print(self.data_dir+'/'+self.file_list[index])
+            print(str(ex))
+            pass
 
     def __len__(self):
         return len(self.file_list)
